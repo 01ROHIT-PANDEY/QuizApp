ac70a4916423e175e36348f906dd91788219a8d4
Rohit <pandeyrohit232@gmail.com>
2024-06-07 10:27:33 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BgToggle from './bgtoggle'
-import { Box, Text } from '@chakra-ui/react'
+import bgquiz from '../Images/bg-quiz.jpeg'
+import CreateQuiz from './createQuiz'
+import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, Center, DarkMode, Divider, Heading, Image, Stack, Text } from '@chakra-ui/react'
 function Home() {
+   const [Quiz,setQuiz]=useState(false);
   return (
-
+   
      <>
-            <Box h='100%' >
+            <Box h='100%' p='5'>
                <Text textAlign="right"><BgToggle/></Text> 
-                <Text fontSize="50px" fontWeight="Bold"textAlign="center">QuizApp</Text>
+
+                
+               <Center>
+                  {Quiz?<CreateQuiz/>:
+                  <DarkMode>
+               <Card maxH='m' maxW='sm' >
+                  {/* <Heading size='xl' fontWeight="Bold"textAlign="center">QuizApp</Heading> */}
+               <CardBody>
+                  <Image
+                     src={bgquiz}
+                     alt=''
+                     borderRadius='sm' 
+                  />
+                  <Stack mt='6' spacing='3'>
+                     <Heading size='md'> Create Your Test</Heading>
+                     
+                  </Stack>
+               </CardBody>
+               <Divider />
+               <CardFooter>
+                  <ButtonGroup spacing='2'>
+                     <Button variant='solid' colorScheme='blue'>
+                     Take Quiz
+                     </Button>
+                     <Button variant='solid' colorScheme='blue' onClick={()=>setQuiz(true)}>
+                     Create Quiz
+                     </Button>
+                  </ButtonGroup>
+               </CardFooter>
+               </Card>
+               </DarkMode>
+               }
+               </Center>
             </Box>
    
         
---
+++ src/components/createQuiz.js
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+function createQuiz() {
+  return (
+    <Box>
+                <Heading>Create Your Questions</Heading>
+    </Box>
+  )
+}
+
+export default createQuiz
